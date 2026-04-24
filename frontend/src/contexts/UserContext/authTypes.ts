944dffb926b9e86f4c18edd94bd3e8db0ec41ae0
Hugo Arthur Bourlot <arthur.hugo962@gmail.com>
2026-04-24 18:28:29 +0100

    Merge branch #i7
    Fixing some errors on settings page

--- frontend/src/contexts/UserContext/authTypes.ts
+++ frontend/src/contexts/UserContext/authTypes.ts
@@ -1,24 +1,25 @@
-export type User = {
-  id: number;
-  name: string | null;
-  username: string;
-  email: string;
-  avatarUrl: string;
-  createdAt: string;
-  updateAt: string;
+export type Score = {
+	wins: number;
+	losses: number;
+	draws: number;
+	elo: number;
+};
 
-  wins: number;
-  losses: number;
-  draws: number;
-  elo: number;
+export type User = {
+	id: number;
+	name: string | null;
+	username: string;
+	email: string;
+	avatarUrl: string;
+	createdAt: string;
+	updateAt: string;
+	boardTheme: 1 | 2 | 3;
+	score: Score | null;
 };
 
 export type AuthState = {
-  user: User | null;
-  isLoading: boolean;
+	user: User | null;
+	isLoading: boolean;
 };
 
-export type AuthAction =
-  | { type: "AUTH_LOADING" }
-  | { type: "AUTH_SUCCESS"; payload: User }
-  | { type: "AUTH_LOGOUT" };
+export type AuthAction = { type: "AUTH_LOADING" } | { type: "AUTH_SUCCESS"; payload: User } | { type: "AUTH_LOGOUT" };
