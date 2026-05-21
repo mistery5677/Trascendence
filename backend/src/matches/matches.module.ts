9c8b07c6d03dc2ce155a535abfd80d6474743bdd
Jorge Marquez <jorgemiliano91@gmail.com>
2026-05-21 12:42:17 +0100

    Backend: infraestrutura em tempo real (WebSockets) #4
    Summary of changes:
    - Remove unused function on matches.controller.ts

--- backend/src/matches/matches.module.ts
+++ backend/src/matches/matches.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-// import { MatchesController } from './matches.controller';
 import { MatchesService } from './matches.service';
 import { PrismaService } from 'src/prisma.service'; // Confirma se o caminho do teu PrismaService é este
 import { MatchesController } from './matches.controller';
