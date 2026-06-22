93e0205b88ceb6a6f8e842fa7bc0050f5cdef961
Jorge Marquez <jorgemiliano91@gmail.com>
2026-06-22 15:03:14 +0100

    Notificacion Bell #35
    Summary of changes:
    - Show only notification bell to logged users

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -141,7 +141,7 @@ export function NavBar({ onModal }: NavBarProps) {
 						</div>
 					</div>
 					<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-						<NotificationBell />
+						{state.user && <NotificationBell />}
 
 						{/* Profile dropdown or Button */}
 						{state.user ? (
